e8f31191130b60e6e67bb11f59c2bb1cd47eb5cc
Jessica Chen <jchen@jchen.local>
2019-03-14 14:34:41 -0500

    added schema, and seeds sql

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("ITSY BITSY TEENY WEENIE POLKA DOT ZUCCHINI BURGER", "FALSE");
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("IF LOOKS CAN KALE BURGER", "FALSE");
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("SUMMER THYME BURGER", "FALSE")
