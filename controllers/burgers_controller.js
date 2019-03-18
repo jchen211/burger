0ca0949eaef16ff32afeb0d1f6f0b2425959f50b
Jessica Chen <jchen@jchen.attlocal.net>
2019-03-17 23:39:19 -0500

    adding burger in progess. eatting burger in progess

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,29 +15,23 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/burgers", function(req, res) {
+router.post("/burgers", function(req, res) {
   burgers.create([
-    "burger_name", "devoured"
+    "burger_name"
   ], [
-    req.body.burger_name, req.body.devoured
+    req.body.burger_name 
   ], function(result) {
-    res.json({ id: result.insertId });
+    res.redirect("/");
   });
 });
 
-router.put("/api/cats/:id", function(req, res) {
+router.put("/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
-  console.log("condition", condition);
-
   burgers.update({
-    devoured: req.body.devoured
+    devoured: true
   }, condition, function(result) {
-    if (result.changedRows == 0) {
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+      res.redirect("/");
   });
 });
 
